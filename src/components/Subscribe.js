19be74e106d1d62c6ac34b816034e9b59d9eaa98
Somethea <sometheasiekswx@gmail.com>
2022-10-04 17:35:15 +1100

    Update Subscribe.js

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import { useForm } from "react-hook-form"
+import { postSubscriber } from "../api"
+import SuccessBox from "./SuccessBox";
 
 
 const Subscribe = () => {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const [success, setSuccess] = useState('')
+
+    const onSubmit = async (data) => {
+        const result = await postSubscriber(data.email)
+        if (result.status === 200) {
+            setSuccess(result.data)
+        }
+        setTimeout(() => {
+            setSuccess('')
+        }, 2000)
+    }
+
     return (
-        <Box sx={{
-            width: {xs: '100%'},
-            py: 1,
-            display: 'flex',
-            justifyContent: {xs: 'space-around', sm: 'center'},
-            alignItems: 'center',
-            gap: {sm: 4}
-        }}>
-            <Typography
-                variant='h6'
-                component={'h6'}
-                href='/'
-                sx={{
-                    textAlign: 'center',
-                    fontFamily: 'monospace',
-                    fontWeight: 500,
-                    letterSpacing: '.1rem',
-                    textDecoration: 'none',
-                    display: {xs: 'none', md: 'flex'}
-                }}
-            >
-                SIGN UP FOR OUT DAILY INSIDER
-            </Typography>
-            <TextField label='Enter your email' variant='outlined' size='small' />
-            <Button variant='contained'>Subscribe</Button>
-        </Box>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {success && <SuccessBox message={success} sx={{m: 0}}/>}
+            <Box sx={{
+                width: {xs: '100%'},
+                py: 1,
+                display: 'flex',
+                justifyContent: {xs: 'space-around', sm: 'center'},
+                alignItems: 'center',
+                gap: {sm: 4}
+            }}>
+
+                <Typography
+                    variant='h6'
+                    component={'h6'}
+                    href='/'
+                    sx={{
+                        textAlign: 'center',
+                        fontFamily: 'monospace',
+                        fontWeight: 500,
+                        letterSpacing: '.1rem',
+                        textDecoration: 'none',
+                        display: {xs: 'none', md: 'flex'}
+                    }}
+                >
+                    SIGN UP FOR OUT DAILY INSIDER
+                </Typography>
+                <TextField label='Enter your email' variant='outlined' size='small' {...register("email")}/>
+                <Button variant='contained' type={'submit'}>Subscribe</Button>
+            </Box>
+        </form>
     )
 }
 
